f4e8df567fe3a93ed42ea5681cd2457800c87746
Marcelo Meirelles <meirellesm@hotmail.com>
2024-01-17 22:22:38 -0300

    feat:initial css and color setup, mock data

---
+++ src/data/data.ts
@@ -0,0 +1,57 @@
+
+export const jsonData = [
+    {
+        alimento:"arroz branco",
+        peso:"500g",
+        modo_consumo:"puro",
+        glicemia:"ficou estável",
+        conclusão:"moderado",
+        hipoglicemia:false
+
+    },
+    {
+        alimento:"doce de leite",
+        peso:"50g",
+        modo_consumo:"puro",
+        glicemia:"subiu",
+        conclusão:"moderado",
+        hipoglicemia:false
+
+    },
+    {
+        alimento:"melancia",
+        peso:"350g",
+        modo_consumo:"puro",
+        glicemia:"subiu 30mg/dl",
+        conclusão:"moderado",
+        hipoglicemia:false
+
+    },
+    {
+        alimento:"macarrão",
+        peso:"500g",
+        modo_consumo:"puro",
+        glicemia:"subiu 50mg/dl",
+        conclusão:"alto",
+        hipoglicemia:false
+
+    },
+    {
+        alimento:"chocolate",
+        peso:"80g",
+        modo_consumo:"puro",
+        glicemia:"subiu 20mg/dl",
+        conclusão:"alto",
+        hipoglicemia:false
+
+    },
+    {
+        alimento:"banana",
+        peso:"100g",
+        modo_consumo:"puro",
+        glicemia:"ficou estável",
+        conclusão:"baixo",
+        hipoglicemia:false
+
+    },
+]
