dae92dc648aacbcab4da18065c37bf8b62247e96
Marcelo Meirelles <meirellesm@hotmail.com>
2024-03-26 19:24:56 -0300

    feat: add admin buttons

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,6 +1,9 @@
+import { useContext } from "react";
 import { IData } from "../../providers/DataContext"
 import "./styles.scss"
 import { FaInstagram } from "react-icons/fa";
+import { RiEditFill, RiDeleteBin6Fill } from "react-icons/ri";
+import { UserContext } from "../../providers/UserContext";
 
 interface ICardProp{
     item: IData
@@ -8,12 +11,19 @@ interface ICardProp{
 
 export function Card({item}:ICardProp){
 
-
+    const { user } = useContext(UserContext)
 
     return (
         <li className="li-card">
             <div>
                 <h4>{item.name.toUpperCase()}</h4>
+                {
+                    user && 
+                    <>
+                        <button><RiEditFill/></button>
+                        <button><RiDeleteBin6Fill/></button>
+                    </>
+                }
                 { item.insta && <a href={item.insta} target="_blank"><FaInstagram size={22} color="purple"/></a> }
             </div>
             <p>Peso: {item.weight}g</p>
