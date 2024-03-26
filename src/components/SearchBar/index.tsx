f2f59bbed27721693475e698c11a8071d7e72be5
Marcelo Meirelles <meirellesm@hotmail.com>
2024-03-26 19:49:56 -0300

    fix:remove unused useState

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext  } from "react";
 import "./styles.scss"
 import { GoSearch } from "react-icons/go";
 import { DataContext } from "../../providers/DataContext";
