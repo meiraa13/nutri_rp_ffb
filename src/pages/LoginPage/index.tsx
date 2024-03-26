9e27c0ba8bbf3273596545d68619c78a917855d8
Marcelo Meirelles <meirellesm@hotmail.com>
2024-03-26 18:43:07 -0300

    feat: add login function

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,9 +1,18 @@
 import { Link } from "react-router-dom"
 import "./styles.scss"
 import { RiArrowGoBackFill } from "react-icons/ri";
+import { useForm } from "react-hook-form";
+import { TLoginData, loginSchema } from "./validator";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useContext } from "react";
+import { UserContext } from "../../providers/UserContext";
+
 
 export function LoginPage(){
-    
+    const { register, handleSubmit } = useForm<TLoginData>({
+        resolver: zodResolver(loginSchema)
+    })
+    const { userLogin  } = useContext(UserContext)
 
     return (
         <div className="div-login">
@@ -12,14 +21,14 @@ export function LoginPage(){
                     <h3>Login</h3>
                     <Link to="/"><RiArrowGoBackFill/></Link>
                 </div>
-                <form className="login-form">
+                <form onSubmit={handleSubmit(userLogin)} className="login-form">
                     <div>
                         <label htmlFor="email">Email</label>
-                        <input type="email" id="email" />
+                        <input type="email" id="email" {...register("email")} />
                     </div>
                     <div>
                         <label htmlFor="password">Senha</label>
-                        <input type="password" id="password" />
+                        <input type="password" id="password" {...register("password")} />
                     </div>
                     <button>Entrar</button>
                 </form>
--- src/providers/UserContext.tsx
+++ src/providers/UserContext.tsx
@@ -1,14 +1,43 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { IChildren } from "./DataContext";
+import { TLoginData } from "../pages/LoginPage/validator";
+import { api } from "../services/api";
+import { useNavigate } from "react-router-dom";
 
+interface IUser {
+    token:string
+}
 
-export const UserContext = createContext({})
+interface IUserContext {
+    user:IUser | null,
+    setUser:React.Dispatch<React.SetStateAction<IUser | null>>,
+    userLogin: (data:TLoginData) =>Promise<void>
+
+}
+
+export const UserContext = createContext({} as IUserContext)
 
 export function UserProvider({children}:IChildren){
+    const [user, setUser] = useState<IUser | null>(null)
+    const navigate = useNavigate()
 
+    async function userLogin(data:TLoginData){
+        try {
+            const response = await api.post("/login", data)
+            setUser(response.data)
+            setTimeout(()=>{
+                navigate("/")
+
+            },1000)
+
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
     
     return (
-        <UserContext.Provider value={{}}>
+        <UserContext.Provider value={{setUser, user, userLogin}}>
             { children }
         </UserContext.Provider>
     )
